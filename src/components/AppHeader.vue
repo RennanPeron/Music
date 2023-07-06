42197c7ce571524b7227c80e56b64f4d8a4ac802
RennanPeron <rennanperon01@gmail.com>
2023-07-06 20:19:29 -0300

    i18n home view

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -25,6 +25,11 @@
                         </li>
                     </template>
                 </ul>
+                <ul class="ml-auto">
+                    <li>
+                        <a class="px-2 text-white" href="#" @click.prevent="changeLocale">{{ currentLocale }}</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </header>
@@ -39,6 +44,9 @@ export default {
     name: "AppHeader",
     computed: {
         ...mapStores(useModalStore, useUserStore),
+        currentLocale() {
+            return this.$i18n.locale === "pt" ? "Português" : "English"
+        }
     },
     methods: {
         toggleAuthModal() {
@@ -50,6 +58,9 @@ export default {
             if (this.$route.name === "manage") {
                 this.$router.push({ name: "home" })
             }
+        },
+        changeLocale() {
+            this.$i18n.locale = this.$i18n.locale === 'pt' ? 'en' : 'pt'
         }
     }
 }
--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -3,7 +3,7 @@ import en from '@/locales/en.json'
 import pt from '@/locales/pt.json'
 
 export default createI18n({
-  locale: 'pt',
+  locale: 'en',
   fallbackLocale: 'en',
   messages: {
     en,
